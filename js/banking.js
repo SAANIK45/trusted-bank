34c62c898212d3ef82098cd64cab0d75f0d8c989
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-16 22:42:59 +0600

    update some issue

--- js/banking.js
+++ js/banking.js
@@ -1,51 +1,59 @@
+//Get input parameter
+function getInput(inputId){
+        //get the deposit input
+        let inputField = document.getElementById(inputId);
+        let newInputText = inputField.value;
+        inputField.value = '';
+        //convert sting to float
+        let newInputAmount = parseFloat(newInputText);
+        return newInputAmount;
+}
+function getTotal(totalFieldId,newDepoAmount){
+        let totalElement = document.getElementById(totalFieldId);
+        let totalText = totalElement.innerText;
+        //convert sting to float
+        let previousAmount = parseFloat(totalText);
+        let newInputTotal = previousAmount + newDepoAmount;
+        totalElement.innerText = newInputTotal;
+}
+function getCurrentBalance(){
+        const balanceField = document.getElementById('main-balance');
+        let newBalanceText = balanceField.innerText;
+        let newBalanceAmount = parseFloat(newBalanceText);
+        return newBalanceAmount;
+}
+function updateBalance(amount, isAdd){
+        const balanceField = document.getElementById('main-balance');
+        const newBalanceAmount = getCurrentBalance();
+        if(isAdd == true){
+            let newBalanceTotal = newBalanceAmount + amount;
+            balanceField.innerText = newBalanceTotal;
+        }else{
+            let newBalanceTotal = newBalanceAmount - amount;
+            balanceField.innerText = newBalanceTotal;
+        }
+}
+
 //Deposit money
 document.getElementById('deposit-btn').addEventListener('click',function(){
-    //get the deposit input
-    let depositField = document.getElementById('deposit-input');
-    let newDepoText = depositField.value;
-    //convert sting to float
-    let newDepoAmount = parseFloat(newDepoText);
-    //deposit total
-    let depositTotal = document.getElementById('deposit-total');
-    let preDepoText = depositTotal.innerText;
-    //convert sting to float
-    let preDepoAmount = parseFloat(preDepoText);
-    let newDepoTotal = preDepoAmount + newDepoAmount;
-    depositTotal.innerText = newDepoTotal;
 
-    //update acc balance
-    const balanceField = document.getElementById('main-balance');
-    let newBalanceText = balanceField.innerText;
-    let newBalanceAmount = parseFloat(newBalanceText);
-
-    let newBalanceTotal = newBalanceAmount + newDepoAmount;
-    balanceField.innerText = newBalanceTotal;
-
-    depositField.value = '';
+    const newDepoAmount = getInput('deposit-input');
+    if(newDepoAmount > 0){
+        getTotal('deposit-total',newDepoAmount);
+        updateBalance(newDepoAmount , true);
+    }
 });
-
 
 //Withdraw money
 document.getElementById('withdraw-btn').addEventListener('click',function(){
     //get withdraw input
-    let withdrawInput = document.getElementById('withdraw-input');
-    let withdrawInputText = withdrawInput.value;
-    let newWithdrawTotal = parseFloat(withdrawInputText);
-    //get withdraw total
-    let withdrawField = document.getElementById('withdraw-total');
-    let newWithdrawText = withdrawField.innerText;
-    let newWithdrawAmount = parseFloat(newWithdrawText);
-    //set total in withdraw field
-    let newWithTotalAmount = newWithdrawTotal + newWithdrawAmount;
-    withdrawField.innerText = newWithTotalAmount;
-    //get main balance
-    const balanceField = document.getElementById('main-balance');
-    let newBalanceText = balanceField.innerText;
-    let newBalanceAmount = parseFloat(newBalanceText);
-    //New balance total
-    let balanceTotal = newBalanceAmount - newWithdrawTotal;
-    balanceField.innerText = balanceTotal;
-    
-    withdrawInput.value = '';
-
-})
+    const newWithdrawTotal = getInput('withdraw-input');
+    const currentBalance = getCurrentBalance();
+    if(newWithdrawTotal > 0 && newWithdrawTotal < currentBalance ){
+        getTotal('withdraw-total', newWithdrawTotal);
+        updateBalance(newWithdrawTotal , false);
+    }
+    if(newWithdrawTotal > currentBalance){
+        alert('Insufficient balance'); 
+    }
+});
